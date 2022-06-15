9f844cfd0e019f02ca3baac550916ef7f33d2a42
sulphurninja <95356721+sulphurninja@users.noreply.github.com>
2022-06-15 17:26:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
     env: {
         "BASE_URL": "https://kaam247.vercel.app",
         "MONGODB_URL": "mongodb+srv://aditya4sure:RiseAbove@work.3smun.mongodb.net/Work?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "Dk8n4%Q_AYcSNDVmbt374PwDX9=G2N=?vE49RpvC",
-        "REFRESH_TOKEN_SECRET": "5$ngjG%y7rhB*Ufm&A$XHFBY?BfT^W+f7a*K3@H@",
+        "ACCESS_TOKEN_SECRET": "&QcnNq?eMyLhyHBD&ZPN_fU-d!9%GAxxfYhRCbZczJK56BT&LY",
+        "REFRESH_TOKEN_SECRET": "N9Jkwc5nATaj*m3^rJv3%P=V&4FyWPVK!A!ZvWMfCFU%+KtfEr",
         "PAYPAL_CLIENT_ID": "AVjDkEiXFmFAEkGDfwpwbngL4uzfGpSj4F4ncJHErbZW6u04Z-kGasPW2A7UTdJdeD9_IdbKQhtazA_c",
         "CLOUD_UPDATE_PRESET": "x8yy4v2u",
         "CLOUD_NAME": "kaam-24x7",
