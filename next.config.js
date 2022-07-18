a0bd89a0b72df2ad0cf31549f8949a91a3a33147
aditya4sure <sulphurninja@gmail.com>
2022-07-18 15:33:11 +0530

    whoah app?

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
@@ -9,18 +8,10 @@ module.exports = withPWA({
     },
 
     env: {
-        "BASE_URL": "http://localhost:3000",
+        "BASE_URL": "https://www.kaam24x7.com",
         "MONGODB_URL": "mongodb+srv://aditya4sure:RiseAbove@work.3smun.mongodb.net/Work?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "Dk8n4%Q_AYcSNDVmbt374PwDX9=G2N=?vE49RpvC",
         "REFRESH_TOKEN_SECRET": "5$ngjG%y7rhB*Ufm&A$XHFBY?BfT^W+f7a*K3@H@",
-=======
-module.exports = {
-    env: {
-        "BASE_URL": "https://kaam247.vercel.app",
-        "MONGODB_URL": "mongodb+srv://aditya4sure:RiseAbove@work.3smun.mongodb.net/Work?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "&QcnNq?eMyLhyHBD&ZPN_fU-d!9%GAxxfYhRCbZczJK56BT&LY",
-        "REFRESH_TOKEN_SECRET": "N9Jkwc5nATaj*m3^rJv3%P=V&4FyWPVK!A!ZvWMfCFU%+KtfEr",
->>>>>>> 9f844cfd0e019f02ca3baac550916ef7f33d2a42
         "PAYPAL_CLIENT_ID": "AVjDkEiXFmFAEkGDfwpwbngL4uzfGpSj4F4ncJHErbZW6u04Z-kGasPW2A7UTdJdeD9_IdbKQhtazA_c",
         "CLOUD_UPDATE_PRESET": "x8yy4v2u",
         "CLOUD_NAME": "kaam-24x7",
